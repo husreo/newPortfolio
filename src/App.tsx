5afe94725dbd48e4f5a85e271f5becca9ffa8661
husreo <springers230@gmail.com>
2024-09-21 05:28:19 -0700

    feat:bg-pattern

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-screen h-screen opacity-50">
+      
     </div>
   );
 }
