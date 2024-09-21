9ccc583d839edfa520aeae9dd489df0be36ce147
husreo <springers230@gmail.com>
2024-09-22 06:13:10 +0900

    update:info&work response

--- src/Page/work.tsx
+++ src/Page/work.tsx
@@ -2,7 +2,7 @@ import { projects } from "../config/menu"
 import { Link } from "react-router-dom"
 
 export const Work = () => {
-    return <div className='py-8 text-white grid grid-cols-2 gap-2'>
+    return <div className='py-8 text-white grid grid-cols-1 md:grid-cols-2 gap-2'>
         { projects.map((val, index) => 
             <div className="border border-dashed border-zinc-700 px-4 py-2 bg-zinc-950 flex flex-col justify-between">
                     <Link to={val.links[0].link} className="text-left text-white hover:underline">{val.title}</Link>
