dcf08d18412ff018e8df270d239aa94893c3e051
husreo <springers230@gmail.com>
2024-09-21 19:00:17 -0700

    update: project target

--- src/Page/work.tsx
+++ src/Page/work.tsx
@@ -5,7 +5,7 @@ export const Work = () => {
     return <div className='py-8 text-white grid grid-cols-1 md:grid-cols-2 gap-2'>
         { projects.map((val, index) => 
             <div className="border border-dashed border-zinc-700 px-4 py-2 bg-zinc-950 flex flex-col justify-between">
-                    <Link to={val.links[0].link} className="text-left text-white hover:underline">{val.title}</Link>
+                    <Link to={val.links[0].link} className="text-left text-white hover:underline" target="block">{val.title}</Link>
                     <img src={val.image} alt="" className="mt-3" />
                     <div className="w-full mt-3">
                         <p className="text-xs text-left text-lime-200">{val.description}</p>
