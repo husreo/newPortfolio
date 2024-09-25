f9886fe617e6ef8b273087c41988e4909d004722
husreo <springers230@gmail.com>
2024-09-25 19:57:18 +0900

    update:iframe-loading

--- src/Page/contact.tsx
+++ src/Page/contact.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+import { FaSpinner } from "react-icons/fa";
+
 export const Contact = () => {
-    
+    const [isLoaded, setIsLoaded] = useState(false);
+
     return <div className='py-8 text-white'>
-        <div className="google-map-code">
-            <iframe
-                src="https://maps.google.com/maps?q=Oudekerksplein+48&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                height="250"
-                className='w-full sm:w-4/5 m-auto'
-                aria-hidden="false"
-                title="google map"
-            />
-        </div>
+            {!isLoaded && (
+            <div className="flex items-center justify-center">
+                <FaSpinner className="animate-spin text-zinc-500" size={20} />
+                <span className="ml-2 text-zinc-500">Loading map...</span>
+            </div>
+            )}
+            <div className="google-map-code">
+                <iframe
+                    src="https://maps.google.com/maps?q=Oudekerksplein+48&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                    height="250"
+                    className='w-full sm:w-4/5 m-auto'
+                    aria-hidden="false"
+                    allowFullScreen={true}
+                    title="google map"
+                    onLoad={() => setIsLoaded(true)}
+                />
+            </div>
     </div> 
 }
